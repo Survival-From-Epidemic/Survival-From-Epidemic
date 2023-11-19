86945dfa60305f0aec8e4f3d77d5049e75ee4c36
Mooner510 <lmck510@gmail.com>
2023-11-19 23:33:29 +0900

    add game manager

---
+++ Assets/_root/Scripts/Game/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using _root.Scripts.Managers;
+using _root.Scripts.Managers.UI;
+using _root.Scripts.SingleTon;
+using UnityEngine;
+
+namespace _root.Scripts.Game
+{
+    public class GameManager : SingleMono<GameManager>
+    {
+        [SerializeField] public GameEndType gameEndType;
+
+        public void GameEnd(GameEndType type)
+        {
+            gameEndType = type;
+            UIManager.Instance.EnableUI(UIElements.GameResult);
+        }
+    }
+}
