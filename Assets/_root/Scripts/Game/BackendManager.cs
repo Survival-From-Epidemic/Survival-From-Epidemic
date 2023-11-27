c60f5938a6d33961804011ab0fec3a936664d9cd
InhyeKang <kanginhae0826@gmail.com>
2023-11-27 13:48:51 +0900

    get progresses, put progresses

--- Assets/_root/Scripts/Game/BackendManager.cs
+++ Assets/_root/Scripts/Game/BackendManager.cs
@@ -1,5 +1,7 @@
-﻿using _root.Scripts.Game.Data;
+﻿using System.Collections;
+using _root.Scripts.Game.Data;
 using _root.Scripts.Managers;
+using _root.Scripts.Network;
 using _root.Scripts.SingleTon;
 using UnityEngine;
 
@@ -8,16 +10,46 @@ namespace _root.Scripts.Game
     public class BackendManager : SingleMono<BackendManager>
     {
         [SerializeField] private KGlobalData kGlobalData;
+        private bool _isLoaded;
+        private int _getCount;
 
         protected override void Awake()
         {
             base.Awake();
+            _isLoaded = false;
+            _getCount = 5;
             RequestData();
+            StartCoroutine(AutoSaving());
+        }
+
+        private IEnumerator AutoSaving()
+        {
+            yield return new WaitUntil(() => _isLoaded);
+            while (true)
+            {
+                yield return new WaitForSecondsRealtime(90);
+                new Networking.Put<KGlobalData>("/progresses", kGlobalData)
+                    .OnError(() =>
+                    {
+                        Debugger.Log("Error!!");
+                    })
+                    .Build();
+            }
         }
 
         private void RequestData()
         {
             //TODO: write code
+            if(_getCount-- <= 0) return;
+            new Networking.Get<KGlobalData>("/progresses")
+                .OnResponse(body =>
+                {
+                    Debugger.Log("");
+                    kGlobalData = body;
+                    _isLoaded = true;
+                })
+                .OnError(RequestData)
+                .Build();
         }
 
         private void RegisterData()
