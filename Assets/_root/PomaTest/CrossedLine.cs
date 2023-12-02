e5d336b23f7452e993fa911db9bffa04b108c84b
Mooner510 <lmck510@gmail.com>
2023-12-02 14:36:54 +0900

    POMA PATHFINDING

---
+++ Assets/_root/PomaTest/CrossedLine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace _root.PomaTest
+{
+    [Serializable]
+    public class CrossedLine
+    {
+        public Vector3 cross;
+        public Line line;
+    }
+}
---
+++ Assets/_root/PomaTest/PomaAnchor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace _root.PomaTest
+{
+    public class PomaAnchor : MonoBehaviour
+    {
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawIcon(transform.position, "flag.png", false);
+        }
+    }
+}
