fe50062d5d35527ceef896751735a53229daffab
lmck510 <lmck510@gmail.com>
2023-12-03 16:36:08 +0900

    add howToPlay

--- Assets/_root/Scripts/UI/GameStartView/GameStartView.cs
+++ Assets/_root/Scripts/UI/GameStartView/GameStartView.cs
@@ -9,9 +9,11 @@ namespace _root.Scripts.UI.GameStartView
     public class GameStartView : RectBackgroundView
     {
         [SerializeField] private Button gameStartButton;
-        [SerializeField] private Button settingButton;
-        [SerializeField] private Button rankButton;
+        [SerializeField] private Button howToPlayButton;
+        [SerializeField] private Button logOutButton;
         [SerializeField] private Button quitButton;
+        [SerializeField] private GameObject howTo;
+        [SerializeField] private int howToIdx;
         private bool _sceneLoading;
 
         private void Start()
@@ -32,6 +34,35 @@ namespace _root.Scripts.UI.GameStartView
                 Application.Quit();
 #endif
             });
+            howToPlayButton.onClick.AddListener(() =>
+            {
+                howToIdx = 0;
+                howTo.gameObject.SetActive(true);
+            });
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+            if (howTo.activeSelf)
+            {
+                if (Input.GetKeyDown(KeyCode.Space))
+                {
+                    howTo.gameObject.SetActive(false);
+                }
+                else if (Input.GetKeyDown(KeyCode.A) && howToIdx > 0)
+                {
+                    howTo.transform.GetChild(howToIdx - 1).gameObject.SetActive(true);
+                    howTo.transform.GetChild(howToIdx).gameObject.SetActive(false);
+                    howToIdx--;
+                }
+                else if (Input.GetKeyDown(KeyCode.D) && howToIdx < howTo.transform.childCount - 1)
+                {
+                    howTo.transform.GetChild(howToIdx + 1).gameObject.SetActive(true);
+                    howTo.transform.GetChild(howToIdx).gameObject.SetActive(false);
+                    howToIdx++;
+                }
+            }
         }
     }
 }
